a88cc7650c7eeb4653e639790c67d68e19257dda
ericjo <eric.johansson@hiq.se>
2019-04-09 18:07:53 +0200

    Nu kör vi

---
+++ src/main/java/se/hiq/hicollege/microservices/HelloWeather.java
@@ -0,0 +1,22 @@
+package se.hiq.hicollege.microservices;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/hello")
+
+
+public class HelloWeather {
+
+}
